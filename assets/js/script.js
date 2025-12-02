2d1e1f2cd75043227ed6186649f2a615dca2cc3c
ithinkufailed <ithinkufailed@gmail.com>
2025-12-02 15:49:45 +0000

    API: Refactor into separate functions for maintainability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,10 @@
 const searchForm = document.getElementById('search-form');
 const searchInput = document.getElementById('search-input');
-let id
-let secret
+let clientId;
+let clientSecret;
 
-// getting a token works
-// when searching an id we can successfully retrieve our artist name
-// neeed to work out how to get artistId from a string name.
-// 
+
 async function getSpotifyToken() {
-    const clientId = id
-    const clientSecret = secret
-
     let url = 'https://accounts.spotify.com/api/token'
 
     return fetch(url, {
@@ -19,7 +13,7 @@ async function getSpotifyToken() {
             "Content-Type": "application/x-www-form-urlencoded" 
             
         },
-        body: `grant_type=client_credentials&client_id=${id}&client_secret=${secret}`
+        body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`
     }).then((response)=>{
         console.log(response, '<-- response')
         return response.json()
@@ -31,8 +25,7 @@ async function getSpotifyToken() {
     })
      
 }
-//fix to search by artist name - dk
-// function to convert name to id which then passed to searchArtist below
+
 async function searchArtist(name) {
     if (name.length === 0) {
         console.log("no name provided")
@@ -41,44 +34,39 @@ async function searchArtist(name) {
 
 
     const token = await getSpotifyToken();
-
     const urlArtist = `https://api.spotify.com/v1/search?q=${name}&type=artist&limit=1`
     const responseArtist = await fetch(urlArtist, {
         headers: { "Authorization": `Bearer ${token}` }
        } 
     );
-
+    
    
 
     const data = await responseArtist.json();
+    const artist = data.artists["items"]
     let artistId = data.artists["items"][0].id
     console.log(artistId)
-    const urlAlbums = `https://api.spotify.com/v1/artists/${artistId}/albums`
-    const responseAlbums = await fetch(urlAlbums, {
-        headers: { "Authorization": `Bearer ${token}` }
-    })
+    const trackArray = await searchArtistTracks(artistId, token);
+    const albumArray = await searchArtistAlbums(artistId, token);
 
-    console.log(responseAlbums, 'Response Albums')
-    if (data.artists.items.length === 0) {
-        console.log("Returned nothing")
-    }
+    const result = {
+        name: artist[0].name,
+        id: artist[0].id,
+        img: artist[0].images,
+        genres: artist[0].genres,
+        followers: artist[0].followers,
+        albums: albumArray,
+        spotify: artist[0].external_urls.spotify,
+        topTracks: trackArray
+    } 
+    return result
+   }
+}
 
-    const artist = data.artists["items"]
-    let albums = await responseAlbums.json()
-    if (albums.length !== 0) {
-        albums = albums["items"]
-    }
-    // Object.entries(albums).forEach(([key, value]) => {
-    //     console.log(key, 'key', value, 'value')
-    // })
-    const albumArray = []
+async function searchArtistTracks(artistId, token) {
+    
     const trackArray = []
     const maxResults = 6;
-    for (let i = 0; i < albums.length && i < maxResults; i++) {
-        console.log(albums[i].name)
-        albumArray.push(albums[i].name)
-        console.log(albumArray)
-    }
 
     const topTracksUrl = `https://api.spotify.com/v1/artists/${artistId}/top-tracks`;
 
@@ -94,32 +82,31 @@ async function searchArtist(name) {
         trackArray.push(topTracksData[i].name)
         console.log(trackArray)
     }
+    console.log("Success! <-- searchArtistTracks")
+    return trackArray;
+}
+
+async function searchArtistAlbums(artistId, token) {
     
-    // console.log(artist, 'Artist')
-    // console.log(data.artists, 'Artists')
-    // console.log(artist[0].name, 'Artist Name') // retrieves name
-    // console.log(artist[0].id, 'Artist ID')
-    // console.log(artist[0].genres, 'Genres')
-    // console.log(artist[0].followers, 'Artist followers')
-    // console.log(albums 'albums')
+    const url = `https://api.spotify.com/v1/artists/${artistId}/albums`
+    const responseAlbums = await fetch(url, {
+        headers: { "Authorization": `Bearer ${token}` }
+    })
 
-    // for (const album of albums) {
-    //     console.log(albuum)
-    // }
+    console.log(responseAlbums, 'Response Albums')
+    let albums = await responseAlbums.json()
+    if (albums.length !== 0) {
+        albums = albums["items"]
+    }
+    const maxResults = 6;
+    const albumArray = []
+        for (let i = 0; i < albums.length && i < maxResults; i++) {
+        console.log(albums[i].name)
+        albumArray.push(albums[i].name)
+        console.log(albumArray)
+    }
 
-    const result = {
-        name: artist[0].name,
-        id: artist[0].id,
-        img: artist[0].images,
-        genres: artist[0].genres,
-        followers: artist[0].followers,
-        albums: albumArray,
-        spotify: artist[0].external_urls.spotify,
-        topTracks: trackArray
-    } 
-   }
-    console.log(result)
-    return result
+
 }
 
 searchForm.addEventListener('submit', function (event) {
